6026656c9acbc2d5d656a5de723f2fed00138baa
Melle Dijkstra <melle.dykstra@gmail.com>
2026-06-25 23:46:47 +0200

    refactor(extension): inherit and re-export updated toolbox Logger

--- apps/extension/src/logger.ts
+++ apps/extension/src/logger.ts
@@ -1,3 +1,5 @@
+import { Logger as BaseLogger } from '@melledijkstra/toolbox'
+
 export function log(...data: unknown[]) {
   if (import.meta.env.MODE === 'development') {
     console.log(...data)
@@ -22,59 +24,18 @@ function debug(...data: unknown[]) {
   }
 }
 
-export class Logger {
-  public name: string
-  public disabled = false
+const customPrinter = {
+  log,
+  error,
+  warn,
+  debug,
+  time: console.time.bind(console),
+  timeEnd: console.timeEnd.bind(console),
+}
 
-  constructor(name: string, disabled = false) {
-    this.name = name
-    this.disabled = disabled
-  }
-
-  log(...data: unknown[]) {
-    if (this.disabled) {
-      return
-    }
-    if (typeof data?.[0] === 'string' && data[0].includes('%c')) {
-      // If the first argument contains a style, we assume it's a styled log
-      // and we use console.log directly to preserve the styles
-      data[0] = `[${this.name}] ${data[0]}`
-      log(...data)
-      return
-    }
-    log(`[${this.name}]`, ...data)
-  }
-
-  error(...data: unknown[]) {
-    error(`[${this.name}]`, ...data)
-  }
-
-  warn(...data: unknown[]) {
-    warn(`[${this.name}]`, ...data)
-  }
-
-  debug(...data: unknown[]) {
-    if (this.disabled) {
-      return
-    }
-    debug(`[${this.name}]`, ...data)
-  }
-
-  private generateLabel(label?: string): string {
-    return label ? `[${this.name}] ${label}` : `[${this.name}]`
-  }
-
-  time(label?: string) {
-    if (this.disabled) {
-      return
-    }
-    console.time(this.generateLabel(label))
-  }
-
-  timeEnd(label?: string) {
-    if (this.disabled) {
-      return
-    }
-    console.timeEnd(this.generateLabel(label))
+export class Logger extends BaseLogger {
+  constructor(name: string, disabled?: boolean) {
+    const isDevelopment = import.meta.env.MODE === 'development'
+    super(name, disabled ?? !isDevelopment, customPrinter)
   }
 }
