4e6675a741b1f1dc30704cb9490bc3d8af33dba7
Melle Dijkstra <melle.dykstra@gmail.com>
2026-08-08 14:14:36 +0200

    fix linting

--- apps/scraper/eslint.config.ts
+++ apps/scraper/eslint.config.ts
@@ -1,8 +1,8 @@
 import { globalIgnores } from 'eslint/config'
 import nodeConfig from '@melledijkstra/config/eslint/node.ts'
 
-export default {
+export default [
   ...nodeConfig,
   // ignore web scripts
-  ...globalIgnores(['scripts/web/**', 'temp/**']),
-}
+  globalIgnores(['storage', 'processing', 'scripts/web/**', 'temp/**']),
+]
