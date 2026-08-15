9411d4472e7af9a2b84a031f13b817403cd7fb09
Melle Dijkstra <melle.dykstra@gmail.com>
2026-08-15 21:26:30 +0200

    sonar default import fix

--- packages/ui/src/svelte/tracker/index.ts
+++ packages/ui/src/svelte/tracker/index.ts
@@ -1,7 +1,7 @@
-import { default as Root } from './Tracker.svelte'
-import { default as Metric } from './TrackerMetric.svelte'
-import { default as Title } from './TrackerTitle.svelte'
-import { default as IconTitle } from './TrackerIconTitle.svelte'
+import Root from './Tracker.svelte'
+import Metric from './TrackerMetric.svelte'
+import Title from './TrackerTitle.svelte'
+import IconTitle from './TrackerIconTitle.svelte'
 
 export const Tracker = {
   Root,
